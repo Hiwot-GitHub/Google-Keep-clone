87b350f8079361af84dbbfeff1a775ae7444f4d9
Sisay Hiwot <angelhiwot@gmail.com>
2023-11-27 18:33:42 +0200

    working on header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        ShuterGrey:'#5F6368',
+        AliceBlue: '#F1F3F4',
+      },
     },
   },
   plugins: [],
