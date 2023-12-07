e6a8ac64c98b298b3dd27edfd1c03a1b90b23bd6
Sisay Hiwot <angelhiwot@gmail.com>
2023-12-07 19:18:36 +0200

    progress on CreatNote

--- app/components/CreateNote.tsx
+++ app/components/CreateNote.tsx
@@ -3,6 +3,9 @@ import {JSXElementConstructor, PromiseLikeOfReactNode, ReactElement, ReactNode, 
 import { POST } from '../Api/Notes/route';
 import { json } from 'stream/consumers';
 import { stringify } from 'querystring';
+import { title } from 'process';
+
+
 
 interface Note{
   id: Number,
@@ -12,11 +15,12 @@ interface Note{
 }
 
 
-
 const  CreateNote = () => {
   const [isFocused, setIsFocused] = useState(false);
   const [inputValue, setInputValue] = useState('');
+  const [title, setTitle] = useState('');
   const [notes, setNotes] = useState<Note[]>([]);
+ 
  
 
   useEffect(() => {
@@ -41,16 +45,20 @@ const  CreateNote = () => {
   const handleInputChange = (event: { target: { value: SetStateAction<string>; }; }) => {
     setInputValue(event.target.value);
   }
+  const handleTitleChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+    setTitle(event.target.value);
+  }
   const handleSubmit = (event: { preventDefault: () => void; }) =>{
     event.preventDefault();
     //I should create note object by sending POST request and calling getNotes function to refresh the data.
+    setIsFocused(false);
     const postNote = async () => {
       try{
         const response = await fetch('http://localhost:3000/Api/Notes', {
           method: 'POST',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
-            title: 'try',
+            title: `${title}`,
             content: `${inputValue}`,
             ownerid: 1
           }),
@@ -67,6 +75,8 @@ const  CreateNote = () => {
       }
     }
     postNote();
+    setInputValue('');
+    setTitle('');
     
   }
 
@@ -79,10 +89,10 @@ const  CreateNote = () => {
             <div className='flex-1 flex flex-col max-w-lg h-auto bg-white mx-auto my-4 rounded-md shadow-lg justify-start items-center'>
                 <form onSubmit={handleSubmit} className='w-full'>
                 <div className={!isFocused ?'hidden':'block'}>
-                <input type='text' id='title' className='w-full px-4 py-2 focus:outline-none' placeholder='Title'/></div>
+                <input type='text' id='title' className='w-full px-4 py-2 focus:outline-none' value={title} onChange={handleTitleChange} placeholder='Title'/></div>
                 <textarea onFocus={() => setIsFocused(true)} id='add-note' rows={1} cols={33} value={inputValue} onChange={handleInputChange}
                  className='w-full h-auto py-2 px-4 focus:outline-none' placeholder='Take a note...' ></textarea>
-                <button type='submit' className='w-10 h-10 bg-AliceBlue rounded-md text-sm'>Submit</button>
+                <div className={!isFocused ?'hidden':'block'}><button type='submit' className='w-10 h-10 bg-AliceBlue rounded-md text-sm'>Submit</button></div>
                 </form>
            </div>  
            <div  className='flex justify-center px-16'>
@@ -102,7 +112,15 @@ const  CreateNote = () => {
 }
 
 const DisplayNote: React.FC<{note: Note}> =({note})  =>{
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const openModal = () =>{
+    setIsModalOpen(true);
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  }
   const handleDelete = (note_id: Number) => {
     const url = `http://localhost:3000/Api/Notes/${note_id.toString()}`;
     
@@ -124,13 +142,18 @@ const DisplayNote: React.FC<{note: Note}> =({note})  =>{
   deleteNote();
 } 
   return (
+    
+   
     <div className="flex-col min-h-[80px] h-auto  flex w-60  shadow-md rounded-md p-1 m-auto text-xs  text-BlackRussian ">
-      <div className='hidden w-[238px] h-[38px] pt-3 px-4 pb-0 '>{note.title}</div>
+     <div className={`${title.length === 0 ? 'hidden': 'block'}`}><div className='w-[238px] h-[38px] pt-3 px-4 pb-0 text-sm '>{note.title}</div></div>
       <div>{note.content}</div>
       <div className='w-[238px] h-[34px] mb-0 mt-[26px] borde-2 border-t-2 '>
         <button onClick={() => handleDelete(note.id)}>Delete</button>
       </div>
       </div>
+    
+   
+    
   );
              
 }
